da6616866ac5bbdb3864bf44cb1b29fc93804878
IQBAL22 <muhamadiqbal9871@gmail.com>
2025-10-17 18:49:44 +0700

    Update

---
+++ script.js
@@ -0,0 +1,14 @@
+const Buka = document.getElementById('Buka');
+const Background = document.querySelector('.Background');
+const Modal = document.querySelector('.isi-modal');
+const Tutup = document.getElementById('Tutup');
+
+Buka.addEventListener('click', () =>{
+    Modal.style.display = 'block';
+    Background.style.display = 'block';
+});
+
+Tutup.addEventListener('click', () =>{
+    Modal.style.display = 'none';
+    Background.style.display = 'none';
+});
